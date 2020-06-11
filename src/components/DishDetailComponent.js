a72efb563d56710e9e47841617ef564f8f369ed8
ritik1jain <ritikjain0706@gmail.com>
2020-06-11 09:40:54 +0530

    assignment 1

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {Card,CardImg,CardBody,CardText,CardTitle} from 'reactstrap';
+
+class DishDetail extends Component{
+
+    constructor(props){
+        super(props);
+
+
+    };
+
+render(){
+    let dish=this.props.dishdetails;
+    let comm;
+
+if(dish!=null){
+    comm=dish.comments.map((idx) => {
+        return (
+        <li key={idx.id} className="list-unstyled">
+            <p>{idx.comment}</p>
+            <p><span>-- </span>{idx.author}<span>,</span>{idx.date.slice(0, 10)}</p>
+        </li>
+        );
+    })
+    return(
+        
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+            <Card>
+                     <CardImg top src={dish.image} alt={dish.name} />
+                     <CardBody>
+                       <CardTitle>{dish.name}</CardTitle>
+                       <CardText>{dish.description}</CardText>
+                     </CardBody>
+             </Card>
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                
+                <h3>Comments</h3>
+                <div list>
+
+                    {comm}
+                </div>
+            </div>
+        </div>
+    );
+        }
+        else{
+            return(<div></div>);
+        }
+    
+};
+
+                            
+    
+
+}
+
+export default DishDetail;
