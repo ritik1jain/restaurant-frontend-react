a55292f461e3ad9a04a0bb5444502ca460ab1413
ritik1jain <ritikjain0706@gmail.com>
2020-07-20 11:58:47 +0530

    Redux thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Loading } from './LoadingComponent';
 import { Card, CardImg, CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import { Link } from 'react-router-dom';
 
@@ -19,15 +19,36 @@ import { Link } from 'react-router-dom';
     }
     
     const Menu = (props) => {
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
                   <RenderMenuItem dish={dish}/>
               </div>
             );
         });
-
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row"> 
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else
         return (
+            
             <div className="container">
                 <div className="row">
                     <Breadcrumb>
