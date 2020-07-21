6e712cbfb64998f05781a747df3f9cc925cd65f9
ritik1jain <ritikjain0706@gmail.com>
2020-07-21 11:53:53 +0530

    promise error handling

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Loading } from './LoadingComponent';
 import { Card, CardImg, CardImgOverlay,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { baseUrl } from '../shared/baseUrl';
     
     function RenderMenuItem({dish}){
         return(
             <Card>
                 <Link to={`/menu/${dish.id}`} >
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
           
